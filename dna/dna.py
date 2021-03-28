de705800ad6d00383a800e37664b5e817ffbc0af
Serwi <serwi0@gmail.com>
2021-03-28 20:20:18 +0200

    Used dedicated library to parse arguments (#15)
    * Used dedicated library to parse arguments
    
    Used argparse library to parse arguments

--- dna/dna.py
+++ dna/dna.py
@@ -1,19 +1,21 @@
 import sys
 import csv
+import argparse
 
-# Checking for correct number of command-line arguments
-if len(sys.argv) != 3:
-    sys.exit("Usage: python dna.py data.csv sequence.txt")
+parser = argparse.ArgumentParser(description = 'Identifies a person based on their DNA', usage = 'python dna.py data.csv sequence.txt' )
+parser.add_argument('database', help = 'CSV file containing STR counts for a list of individuals')
+parser.add_argument('dna_sequence', help = 'text file containing dna to analyze')
+args = parser.parse_args()
 
 # Loading data from CSV file into memory as List of Dictionaries
 people = []
-with open(sys.argv[1]) as file:
+with open(args.database) as file:
     reader = csv.DictReader(file)
     for row in reader:
         people.append(row)
 
 # Loading sequence from TXT file into memory as String
-with open(sys.argv[2]) as file:
+with open(args.dna_sequence) as file:
     dna = file.read()
 
 # Making Dictionary for dna_strs
